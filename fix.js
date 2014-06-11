9e8ce00b7440265be5d5cad7e113d23db84282e2
Add Chan <addison95132@gmail.com>
2014-06-10 18:09:32 -0700

    Update fix.js

--- fix.js
+++ fix.js
@@ -2,7 +2,7 @@ alert("Welcome to the Computer Fixer!")
 var help = prompt("What do you need help with? \n 1. Printer issues. \n 2. Internet issues. \n 3. Something else")
 console.log(help)
 var x = false
-for (x){
+while (x){
 switch (help){
   case "1":
     var printer = prompt("What happened? \n 1.")
