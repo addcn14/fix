85ca850adc56c4cc1114ce0ade98d03858e30352
Add Chan <addison95132@gmail.com>
2014-06-10 18:05:25 -0700

    Update fix.js

--- fix.js
+++ fix.js
@@ -1,5 +1,6 @@
+alert("Welcome to the Computer Fixer!")
 var help = prompt("What do you need help with? \n 1. Printer issues. \n 2. Internet issues. \n 3. Something else")
-/*switch (help){
+switch (help){
   case "1":
     var printer = prompt("What happened? \n 1.")
     break;
@@ -12,4 +13,4 @@ var help = prompt("What do you need help with? \n 1. Printer issues. \n 2. Inter
   default:
     alert('Sorry! That was not a choice. \n (Remember not to include the "." with the number.')
 }
-*/
+
