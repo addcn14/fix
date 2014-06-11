885814cc485421d5fb1d237a9fcf0964c273052b
Add Chan <addison95132@gmail.com>
2014-06-10 18:10:08 -0700

    Update fix.js

--- fix.js
+++ fix.js
@@ -1,20 +1,20 @@
 alert("Welcome to the Computer Fixer!")
 var help = prompt("What do you need help with? \n 1. Printer issues. \n 2. Internet issues. \n 3. Something else")
 console.log(help)
-var x = false
-while (x){
+var x = 0
+while (x === 0){
 switch (help){
   case "1":
     var printer = prompt("What happened? \n 1.")
-    x=true
+    x=1
     break;
   case "2":
     
-    x=true
+    x=1
     break;
   case "3":
     
-    x=true
+    x=1
     break;
   default:
     alert('Sorry! That was not a choice. \n (Remember not to include the "." with the number.')
