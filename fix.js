d58b33ace237257683bae0e5b49dc339f8460f0a
addcn14 <addison95132@gmail.com>
2014-07-07 09:35:34 -0700

    First

--- fix.js
+++ fix.js
@@ -1,23 +1,20 @@
-alert("Welcome to the Computer Fixer!")
-var x = 0
-while (x === 0){
-var help = prompt("What do you need help with? \n 1. Printer issues. \n 2. Internet issues. \n 3. Something else")
-switch (help){
-  case "1":
-    var printer = prompt("What happened? \n 1.")
-    x=1
-    break;
-  case "2":
-    
-    x=1
-    break;
-  case "3":
-    
-    x=1
-    break;
-  case null:
-    x=1
-  default:
-    alert('Sorry! That was not a choice. \n (Remember not to include the "." with the number.')
-}
-}
+$(document).ready(function(){
+	$('#button').click(function(){
+		alert("yay!")
+		var yes = true
+		while (yes === true) {
+			var input = prompt("What is your computer problem?\nType in 1 for printer problems\nType in 2 for network problems")
+				switch(input){
+					case ("1"):
+						var print = prompt("Is the printer printing?\n1 for yes, and 2 for no.").toUpperCase()
+							//if (print = )
+						break;
+					case("2"):
+						var network = prompt("Can you access Google.com?\n1 for yes and 2 for no.")
+						break;
+					default:
+						alert("Sorry. That was not a choice.")
+			}
+		}
+	})
+})
